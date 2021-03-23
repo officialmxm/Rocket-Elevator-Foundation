d32cd0102fe208d5106215b15b5ee5b46d6a6dc4
officialmxm <dubb3rs1ck@icloud.com>
2021-03-23 18:28:00 -0400

    work

--- app/controllers/building_controller.rb
+++ app/controllers/building_controller.rb
@@ -2,13 +2,12 @@ class BuildingController < ApplicationController
   def customer
     @customer = Customer.new
     @customers = Customer.all
-    @buildings = []
     @customerId = params[:customer_id].to_s
+    @buildings = []
 
-    if @CustomerId != " "
+    if @customerId != " "
        Building.where(customer_id: @customerId).each do |b|
         @buildings.append(b.id)
-        puts @CustomerId
       end
     end
     if request.xhr?
